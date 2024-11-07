7c96db2a95d089970ee62aa9c214aa8ebadac9ef
Lina <Linapovrychkina@gmail.com>
2024-11-07 10:58:56 +0700

    группировка и агрегатные функции

--- operators_in_between_.sql
+++ operators_in_between_.sql
@@ -97,4 +97,107 @@ WHERE
 
 
 
+ELECT 
+    sum(*)
+FROM
+    sales
+    
+    /*подсет количества строк за определенный период*/
+SELECT 
+    COUNT(*)
+FROM
+    sales
+WHERE
+    sdate > '2024-09-01'
+        AND sdate <= '2024-09-30';
+	       
+	       /*среднее значение за период*/
+SELECT 
+    AVG(amount)
+FROM
+    sales
+WHERE
+    sdate > '2024-09-01'
+        AND sdate <= '2024-09-30';
+	   
+	   /*среднее*/
+  SELECT 
+    AVG(sdate)
+FROM
+    sales
+WHERE
+    sdate > '2024-09-01'
+        AND sdate <= '2024-09-30';
 
+	/*при использовании distinct внутри функции COUNT()
+подсчитываются только уникальные значения в столбце */
+SELECT 
+    COUNT(distinct city)
+FROM
+    customers;
+    
+    /* null- значения игнорируются при подсчете значений по одному столбцу*/
+SELECT 
+    COUNT(rating)
+FROM
+    customers;
+  
+  /*фяункция max и min работают со строковыми значениями, 
+  ориентируются на алфаитный порядок*/
+SELECT 
+    Min(cname)
+FROM
+    customers;
+    
+    /*группировка максимальных значений продаж по каждому продавцу*/
+SELECT 
+    snum, MAX(amount)
+FROM
+    sales
+GROUP BY snum;
+    
+        /*группировка значений по нескольким столбцам*/
+SELECT 
+    snum, sdate, MAX(amount)
+FROM
+    sales
+GROUP BY snum , sdate;
+    
+/*отбор значений после группировки
+ осуществляетсяс помощью специального выраения having.
+ where отбирает строки до группировки и не может содержать агрегатные функции*/
+SELECT 
+    snum, sdate, MAX(amount)
+FROM
+    sales
+GROUP BY snum , sdate
+HAVING MAX(amount)>1000;
+    
+    
+    /*HAVING  можно использовать только для сгруппированных столбцов */
+SELECT 
+    snum, sdate, MAX(amount)
+FROM
+    sales
+GROUP BY snum , sdate
+HAVING sdate > '2024-09-15';    
+    
+   /*если в группировке не используется поле,
+   по которому нужно сделать отбор то исппользуется только выражение where */
+SELECT 
+    snum, sdate, MAX(amount)
+FROM
+    sales
+WHERE
+    sdate > '2024-09-15'
+GROUP BY snum , sdate;  
+
+/*использование in с having*/  
+SELECT 
+    snum, sdate, MAX(amount)
+FROM
+    sales
+WHERE
+    sdate > '2024-09-15'
+GROUP BY snum
+HAVING snum IN (101 , 106); 
